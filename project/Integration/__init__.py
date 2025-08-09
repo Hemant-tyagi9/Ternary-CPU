97f4b27b27ef305f4c950b467ff3492a4bd8fe23
Hemant tyagi <hemantmu85@gmail.com>
2025-08-09 15:51:07 +0530

    Delete project directory

--- project/Integration/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import cpu_extend
-from . import hardware_sim
-
-__all__ = ['cpu_extend', 'hardware_sim']
--- project/trincore_applications/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import config
-from . import logging
-
-__all__ = ['config', 'logging']
